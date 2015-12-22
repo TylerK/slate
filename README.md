ce3c385fd507e439e6ea9f5dc13c685173f43d7a
Tyler Kelley <tylerckelley@gmail.com>
2015-12-22 14:43:15 -0800

    Updating readme; bootstraping the application;

---
+++ README.md
@@ -0,0 +1,30 @@
+## Slate UI
+Unopinionated React UI components with a slightly opinionated style.
+
+## Components
+* **Blocks**
+    - [ ] Buttons
+    - [ ] Links
+    - [ ] Panels
+    - [ ] Tooltips
+    - [ ] Headings
+    - [ ] Icons
+    - [ ] Lists (vertical & horizontal)
+* **Components**
+    - [ ] Cards
+    - [ ] Dropdowns
+    - [ ] Text inputs
+    - [ ] Date inputs
+    - [ ] Checkboxes & Radios
+    - [ ] Range sliders
+    - [ ] Notifications
+    - [ ] Wizard steps
+    - [ ] Navigation Bar
+
+## Contribute
+```
+git clone https://github.com/TylerK/slate.git
+npm install
+npm start
+http://localhost:8080
+```
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules|dist/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!sass-loader')
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('index.css')
+  ],
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './src')]
+  }
+};
